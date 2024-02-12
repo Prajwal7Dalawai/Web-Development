0671e877564ec249df816a7a00155a978215290e
Prajwal7Dalawai <prajwaldalawai123@gmail.com>
2024-02-12 18:10:54 +0530

    New commits

---
+++ Javascript/String Methods.js
@@ -0,0 +1,14 @@
+//Trim method in JS
+var name="     Prajwal Dalawai     "
+console.log(name.trim());
+
+//toUpperCase() method in string
+name=name.trim()
+console.log(name);
+console.log(name.toUpperCase())
+var upper=name.toUpperCase()
+console.log(upper)
+
+//to LowerCase() method 
+lower=name.toLowerCase()
+console.log(lower)
