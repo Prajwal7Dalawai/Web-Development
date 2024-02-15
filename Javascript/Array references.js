8d9432d772e82824aa9b70eea01e90f56b122174
Prajwal7Dalawai <prajwaldalawai123@gmail.com>
2024-02-16 01:13:41 +0530

    Changes

---
+++ Javascript/Array references.js
@@ -0,0 +1,9 @@
+array1=[1,2,3,4,5]; //different address
+array2=[1,2,3,4,5];  //different address
+//array1===array2 ==>False
+//array name always stores the address.
+
+newarray=array1;
+//array1===newarray ==> True
+//[==]-> compares two values
+//[===]-> compare two data types
