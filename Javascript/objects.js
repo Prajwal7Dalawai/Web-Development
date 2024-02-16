e33962dc49bb99e83df64f8dee4f368178290b2a
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-16 11:47:12 +0530

    Objects in JS

--- Javascript/objects.js
+++ Javascript/objects.js
@@ -8,3 +8,12 @@ var person={
 console.log(person.name," ",person.age);
 person.sayhello();
 
+const post = {
+    name1:"Prajwal Dalawai",
+    username:"@prajwal_dalawai",
+    caption : "This is my first post",
+    likes: 500,
+    reposts: 2,
+    tags: ["@apnacollege","@delta"],
+}
+console.log(post)
