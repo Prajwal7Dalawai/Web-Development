b492807ec2e86122e8e2f72b8a326c8316bcd112
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-16 18:47:34 +0530

    Functions in JS

---
+++ Javascript/Functions.js
@@ -0,0 +1,16 @@
+function print1to5()
+{
+    for(let i=1;i<=5;i++)
+    console.log(i);
+}
+
+function isadult(age){
+    if(age >= 18)
+    console.log("Adult");
+else
+    console.log("Not an adult");
+}
+
+print1to5();
+isadult(19);
+
