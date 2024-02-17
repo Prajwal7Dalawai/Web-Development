30891134fe34904a0b245dcb4dbf04940e2c7e1a
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-17 21:07:12 +0530

    changes

--- Javascript/Map.js
+++ Javascript/Map.js
@@ -9,3 +9,13 @@ const flavour=Pizza.map(pizza=>pizza.flavour);
 console.log(flavour);
 const num=[1,2,3,4,5,6];
 console.log(num.map(nu=>nu*2));
+
+let arr=[1,2,3,4];
+let arr1 = arr.map((el)=>{
+    return el**2;
+});
+console.log(arr1);
+let Price = Pizza.map((el)=>{
+    return el.price + (el.price*(18/100));
+});
+console.log(Price);
