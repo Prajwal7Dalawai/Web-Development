6a1fe34b61d370e8a660acbc83a258ae39510e8b
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-17 22:44:09 +0530

    changes

---
+++ Javascript/spread(array & objects).js
@@ -0,0 +1,11 @@
+let arr1=[1,2,3,4,5];
+let arr2=[6,7,8,9,10];
+let newarr=[...arr1, ...arr2];
+console.log(...newarr);
+
+let Student={
+    name:"Prajwal Dalawai",
+    USN: "2SD22CS058",
+};
+const data={...Student, marks:90.5};
+console.log(data);
