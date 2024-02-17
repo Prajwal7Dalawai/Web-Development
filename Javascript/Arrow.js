401006c2654eaae3a55cbb0cfb37360c68ad9118
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-17 12:07:39 +0530

    tRY AND cATCH

---
+++ Javascript/Arrow.js
@@ -0,0 +1,13 @@
+function good()
+{
+    console.log("Good morning Nivvi");
+};
+good();
+
+const greet=(name)=>{
+    console.log("Good morning "+name);
+};
+greet("Prajwal");
+const great=(name)=>"Good night "+name;
+let l=great("Ankya");
+console.log(l);
