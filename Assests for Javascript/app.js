0904a35416bc5bff8fb79b03d00412694e260f28
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-19 22:12:53 +0530

    Changes

--- Assests for Javascript/app.js
+++ Assests for Javascript/app.js
@@ -1,3 +1,14 @@
-let smallimages = document.getElementsByClassName("oldImg");
-for(let i = 0; i<smallimages.length; i++)
-console.dir(smallimages[i].src);
+let btns = document.querySelectorAll('button');
+let btn;
+for(btn of btns){
+  // btn.addEventListener('click',sayhello);
+   btn.addEventListener('mouseenter',sayname);
+   btn.addEventListener('mouseleave',sayhello);
+}
+function sayhello(){
+    alert("Hello Brother, why did u leave me?"); 
+}
+function sayname()
+{
+    alert("Don't you dare to click that button. You will be hacked");
+}
