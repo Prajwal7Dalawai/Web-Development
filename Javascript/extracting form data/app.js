72522cfdc71ff7e89a57d85856bc914e5992dbb2
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-28 23:13:33 +0530

    New files

--- Javascript/extracting form data/app.js
+++ Javascript/extracting form data/app.js
@@ -1,7 +1,18 @@
 let form = document.querySelector('form');
 form.addEventListener('submit',function(event) {
     event.preventDefault();
-    let inp = document.querySelector('input');
+   /* let inp = document.querySelector('input');
     console.dir(inp);
-    console.log(inp.value);
-})
+    console.log(inp.value);*/
+    /*let user = document.querySelector('#user');
+    let pass = document.querySelector('#pass');
+    console.log(user.value);
+    console.log(pass.value);
+    alert(`Hello ${user.value}, your password has been set as ${pass.value}.`);*/
+    console.dir(this);
+    console.log(this.elements[0].value);
+    console.log(this.elements[1].value);
+    let user = this.elements[0];
+    let pass = this.elements[1];
+    alert(`Hello ${user.value}, your password has been set to ${pass.value}.`);
+});
