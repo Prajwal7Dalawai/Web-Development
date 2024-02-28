1be730d2ab50a1217e788761666f3c376fc64a77
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-29 00:41:20 +0530

    Changes

--- Javascript/Change event/button activity/app.js
+++ Javascript/Change event/button activity/app.js
@@ -1,1 +1,19 @@
-let btn = document.createElement('button');
+/*let btn = document.createElement('button');
+let div = document.querySelector('div');
+div.appendChild(btn)
+btn.innerText= "new button";
+btn.addEventListener('click',()=>{
+    btn.style.backgroundColor= "pink";
+    div.style.backgroundColor= "yellow";
+});*/
+let inp = document.querySelector('input');
+inp.addEventListener("input",()=>{
+    let s = inp.value;
+    let l = s.length;
+    let end = s[l-1];
+    if(!(end>='a' && end<='z') || (end>='A' && end<='Z') || end == ' ')
+    {
+        s.pop();
+        inp.innerText=s;
+    }
+});
