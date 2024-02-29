b6be2063e5ed06170f08405bcab5d26ce7af5f2d
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-02-29 21:48:10 +0530

    New additions

--- Projects/To-Do list application/app.js
+++ Projects/To-Do list application/app.js
@@ -1,8 +1,19 @@
 let save = document.querySelector('#submit');
-save.addEventListener('click',function() {
-
+let title = document.querySelector('#title');
+save.addEventListener('click',function(event) {
+    console.log(title.value);
+    console.dir(form);
+    event.preventDefault();
+   filldata();
 });
-let form = document.querySelector('form')
+let form = document.querySelector('form');
+let table = document.querySelector('table');
+
 function filldata(){
+let tr = document.createElement('tr');
+let td1 = document.createElement('td');
+td1.innerText =title.value ;
+table.insertAdjacentElement('beforeend',tr);
+tr.insertAdjacentElement('afterbegin',td1);
 
 }
