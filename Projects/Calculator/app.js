5fc9025aee6d0a8c417052ab5ff9059e76b6f7c9
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-03-20 09:17:30 +0530

    Calculator

--- Projects/Calculator/app.js
+++ Projects/Calculator/app.js
@@ -1,16 +1,34 @@
 let keys = document.querySelectorAll('.keys');
-let show = document.querySelectorAll('.show');
-show.innerText = ' ';
+let show = document.querySelector('.show');
+let equal = document.querySelector('.eq');
+let seq = [];
+show.innerText= '';
 keys.forEach(key=>{
     key.addEventListener('click',()=>{
-        show.innerText = show.innerText + key.innerText;
+    show.innerText = show.innerText + key.innerText;
+    seq.push(key.innerText);
+    console.log(seq);
     });
 });
-
+equal.addEventListener('click',()=>{
+if(seq.length<3){
+    alert("Enter The operands to calculate");
+}
+else{
+    var num1 = seq[0];
+    var num2 = seq[2];
+    show.innerText=(calculate(num1,seq[1],num2));
+    //reset();
+}
+});
+function reset(){
+    seq = [];
+    show.innerText = '';
+}
 function calculate(op1,op,op2){
     switch(op)
     {
-        case 'X': return op1*op2;
+        case 'X': return Math.imul(op1,op2);
                     break;
 
         case '+': return op1+op2;
