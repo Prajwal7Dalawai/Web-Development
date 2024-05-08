db93893b1a56e50c04c6c7d679d03013afceec13
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-05-08 22:45:17 +0530

    Listening to the port

---
+++ Back-end/ExpressDir/app.listen.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+//console.dir(app);
+let port = 8080;
+app.listen(port , ()=>{
+    console.log("You are listening to " + port + " port");
+});
+
+app.use((req,res)=>{
+    //console.log(req);
+    console.log("Request recieved");
+    //res.send("This is a basic response");
+//     res.send({
+//         name:"Apple",
+//         color:"Silver",
+//         processor:"i7"
+//     });
+
+res.send("<h2>This is my first webpage!!</h2><h3>This is my first webpage Prajwal!!</h3><h4>This is my first webpage!!</h4><h5>This is my first webpage!!</h5><h6>This is my first webpage!!</h6>")
+ });
