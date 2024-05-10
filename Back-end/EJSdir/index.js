78f4d8f1e116cb5e78f4bdda864d7b970154100f
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-05-10 12:14:02 +0530

    Rendering Webpage in EJS format as a server response

--- Back-end/EJSdir/index.js
+++ Back-end/EJSdir/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 8080;
+const path = require("path");
 //Express automatically requires EJS, Hence no need to require EJS seperately.
 app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views")); //This is used when we try to start our server from the parent directory.
+app.get("/",(req,res)=>{
+    res.render("homepage/home");
+});
 
 app.listen(port, (re) => {
     console.log(`Example app listening on port ${port}!`);
