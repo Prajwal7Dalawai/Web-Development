2ba529a7084b8513eac9a93245d8206af82bfcc8
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-07-11 11:51:04 +0530

    added node modules

--- Projects/Praj GPT/gemini.js
+++ Projects/Praj GPT/gemini.js
@@ -13,15 +13,17 @@ const model = genAI.getGenerativeModel({
 // const res = await model.generateContent("Top 10 Programming Languages in boom");
 
 // console.log(res.response.text());
-
+let cont = document.querySelector('.container');
 
 let respons = document.getElementById('response');
 
 frm.addEventListener('submit',async (e)=>{
     e.preventDefault();
     console.log(prmt.value);
-    respons.style.visibility = "visible";
+    cont.style.visibility = "visible";
     const res = await model.generateContent(prmt.value);
+    cont.style.visibility = "hidden";
     const cleanedres = res.response.text().replace(/(\*\*|\#|\*)/g, "","");
+    respons.style.visibility = "visible";
     respons.innerText = cleanedres;
 });
